446afffea69ce25c44a45a76420ada05530b29c5
Rafiullah08 <140521887+Rafiullah08@users.noreply.github.com>
2023-12-01 01:02:03 +0500

    coin base clone

---
+++ coin base/app.js
@@ -0,0 +1,19 @@
+alert("rgc")
+
+
+
+var num1 = 98.987
+var roundnum = Math.round(num1)
+console.log(num1)
+console.log(roundnum)
+
+var num1 = 98.987
+var floornum = Math.floor(num1)
+console.log(num1)
+console.log(floornum)
+
+
+var num1 = 98.987
+var cielnum = Math.ceil(num1)
+console.log(num1)
+console.log(cielnum)
